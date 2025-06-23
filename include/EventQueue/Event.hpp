c0f77d10047221deb90ac4c1c9720d7151a4aedb
Huanming Song <112418903+Nono-Song@users.noreply.github.com>
2025-06-23 15:06:11 -0400

    fix: Make fields of all Event class const

--- include/EventQueue/Event.hpp
+++ include/EventQueue/Event.hpp
@@ -20,26 +20,26 @@ struct PlayEvent
     explicit PlayEvent(const identifier_type id, filename_type path)
     : id(id), filename(std::move(path)) {}
 
-    identifier_type id;
-    filename_type filename;
+    const identifier_type id;
+    const filename_type filename;
 };
 
 struct PauseEvent
 {
     explicit PauseEvent(const identifier_type id): id(id) {}
-    identifier_type id{};
+    const identifier_type id{};
 };
 
 struct ResumeEvent
 {
     explicit ResumeEvent(const identifier_type id): id(id) {}
-    identifier_type id;
+    const identifier_type id;
 };
 
 struct StopEvent
 {
     explicit StopEvent(const identifier_type id): id(id) {}
-    identifier_type id;
+    const identifier_type id;
 };
 
 struct AudioReadyEvent {};
@@ -47,15 +47,15 @@ struct AudioReadyEvent {};
 struct AudioFinishedEvent
 {
     explicit AudioFinishedEvent(const identifier_type id): id(id) {}
-    identifier_type id;
+    const identifier_type id;
 };
 
 struct AudioErrorEvent
 {
     AudioErrorEvent(const std::optional<identifier_type>& id, std::string s)
     : id(id), error_msg(std::move(s)) {}
-    std::optional<identifier_type> id;
-    std::string error_msg;
+    const std::optional<identifier_type> id;
+    const std::string error_msg;
 };
 
 struct ShutdownEvent {};
