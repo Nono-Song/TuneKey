dd897128a5d6001ce58b3a0781fc7e35a8bfa383
Huanming Song <112418903+Nono-Song@users.noreply.github.com>
2025-06-26 12:39:01 -0400

    refactor(audio): Deleted source file of IAudioController by moving the destructor definition to the header file.

--- include/IAudioController/IAudioController.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by Schizoneurax on 3/18/2025.
-//
-
-#include "IAudioController.hpp"
-
-IAudioController::~IAudioController() = default;
--- include/IAudioController/IAudioController.hpp
+++ include/IAudioController/IAudioController.hpp
@@ -8,7 +8,7 @@
 //@formatter:off
 struct IAudioController
 {
-    virtual ~IAudioController();
+    virtual ~IAudioController() = default;
     virtual void start() = 0;
     virtual void shutdown() = 0;
 
